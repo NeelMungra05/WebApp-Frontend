669114a53a9dfcee28ac50b3704acf0818070564
NeelMungra05 <nmungra@deloitte.com>
2023-06-05 17:31:16 +0530

    Added home to nav link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Header = (props) => {
   return (
     <header>
+      <h1>Logo</h1>
       <nav>
         <ul>
+          <li>Home</li>
           <li>Login</li>
           <li>Register</li>
         </ul>
