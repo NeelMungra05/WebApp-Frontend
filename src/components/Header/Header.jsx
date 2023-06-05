ac583a01edfccd3054b68173e0031ecf4b84dcf0
NeelMungra05 <nmungra@deloitte.com>
2023-06-05 16:02:43 +0530

    Added header logic and UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = (props) => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Login</li>
+          <li>Register</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
