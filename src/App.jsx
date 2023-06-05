1ddd515bc61b177857592e5a56a95b24a41ff559
NeelMungra05 <nmungra@deloitte.com>
2023-06-05 16:02:34 +0530

    Seprated out header part to Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import { useState } from "react";
 import Forms from "./components/Form/Forms";
+import Header from "./components/Header/Header";
 
 function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const Header = (props) => {
-    return (
-      <header>
-        <nav>
-          <ul>
-            <li>Login</li>
-            <li>Register</li>
-          </ul>
-        </nav>
-      </header>
-    );
-  };
 
   return (
     <>
