a812b1d508bf908b42da419ce5e9935c70034b57
NeelMungra05 <nmungra@deloitte.com>
2023-06-05 21:41:04 +0530

    Added classes to elements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 const Header = (props) => {
   return (
-    <header>
-      <h1>Logo</h1>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Login</li>
-          <li>Register</li>
+    <header className={styles.header}>
+      <h1 className={styles.header__logo}>Logo</h1>
+      <nav className={styles.header__nav}>
+        <ul className={styles.header__links}>
+          <li className={styles["header__links--active"]}>Home</li>
+          <li className={styles["header__links--item"]}>Login</li>
+          <li className={styles["header__links--item"]}>Register</li>
         </ul>
       </nav>
     </header>
