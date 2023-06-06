e0fb9e8d66ce2ab5607455e5b9e27e51a59ad2bf
NeelMungra05 <nmungra@deloitte.com>
2023-06-06 12:24:24 +0530

    Removed unwanted states

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import Forms from "./components/Form/Forms";
 import Header from "./components/Header/Header";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   return (
     <>
       <Header />
