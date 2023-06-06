c4c2a611e50f68281aefde116644a91b44310670
NeelMungra05 <nmungra@deloitte.com>
2023-06-06 16:26:59 +0530

    Added state to navigate between the navbar option

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,39 @@ import Forms from "./components/Form/Forms";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [isHome, setIsHome] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
+
+  const navbarStateChanger = (navState) => {
+    switch (navState) {
+      case "home":
+        setIsHome(true);
+        setIsLogin(false);
+        setIsRegister(false);
+        break;
+      case "register":
+        setIsRegister(true);
+        setIsHome(false);
+        setIsLogin(false);
+        break;
+      case "login":
+        setIsLogin(true);
+        setIsHome(false);
+        setIsRegister(false);
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <Header />
+      <Header
+        home={isHome}
+        login={isLogin}
+        register={isRegister}
+        navbarStateChanger={navbarStateChanger}
+      />
       <Forms />
     </>
   );
