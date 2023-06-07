6cb2534791a801916bf5d448483539a42128d14e
NeelMungra05 <nmungra@deloitte.com>
2023-06-07 17:06:26 +0530

    Added forwardRef

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div>
       <label htmlFor="">Username </label>
       <input type="text" />
     </div>
   );
-};
+});
 
 export default Input;
