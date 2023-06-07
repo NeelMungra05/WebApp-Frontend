32a81f044944b24e29d3df9c20dbf9a038c60a4e
NeelMungra05 <nmungra@deloitte.com>
2023-06-07 21:40:41 +0530

    Added ref for username and password and also passed the credentials to loginHandler recieved via props

--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { useRef } from "react";
 import Input from "../UI/Input";
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const loginButtonHandler = (e) => {
+    e.preventDefault();
+
+    const username = usernameInputRef.current.value;
+    const password = passwordInputRef.current.value;
+
+    props.loginHandler({
+      username,
+      password,
+    });
+  };
+
   return (
     <>
       <form action="">
         <Input
           label="Username"
+          ref={usernameInputRef}
           input={{
             id: "username",
             type: "text",
@@ -14,11 +31,15 @@ const LoginForm = () => {
         />
         <Input
           label="Password"
+          ref={passwordInputRef}
           input={{
             id: "password",
             type: "password",
           }}
         />
+        <button onClick={loginButtonHandler} type="submit">
+          Login
+        </button>
       </form>
       <p>
         Not Registered ! <a href="">Register here</a>
