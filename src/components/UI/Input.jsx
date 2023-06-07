8340e3d7e0551484d470e17ca2861d68b7214aa2
NeelMungra05 <nmungra@deloitte.com>
2023-06-07 17:04:08 +0530

    Bone structure for Input

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = (props) => {
+  return (
+    <div>
+      <label htmlFor="">Username </label>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Input;
