66edbd4856d7fe49058fbdf944fc78f0b84bff98
NeelMungra05 <nmungra@deloitte.com>
2023-06-08 10:15:23 +0530

    Separated out navbar logic according to isLoggedIn state

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./Header.module.css";
 
 const Header = (props) => {
+  console.log("This is header.jsx");
+
   const navClickHandler = (e) => {
     e.preventDefault();
     console.log(e.target.innerText.toLowerCase());
@@ -11,37 +13,52 @@ const Header = (props) => {
   const navActiveStyle = `${styles.nav__link} ${styles["nav__link--active"]}`;
   const navStyle = styles.nav__link;
 
+  let navbar = (
+    <nav className={styles["nav"]}>
+      <a
+        onClick={navClickHandler}
+        href="/home"
+        className={props.home ? navActiveStyle : navStyle}
+      >
+        <span className={styles.nav__item}>Home</span>
+      </a>
+
+      <a
+        onClick={navClickHandler}
+        href="/login"
+        className={props.login ? navActiveStyle : navStyle}
+      >
+        <span className={styles.nav__item}>Login</span>
+      </a>
+
+      <a
+        onClick={navClickHandler}
+        href="/register"
+        className={props.register ? navActiveStyle : navStyle}
+      >
+        <span className={styles.nav__item}>Register</span>
+      </a>
+    </nav>
+  );
+
+  if (props.isLoggedIn) {
+    console.log(props.isLoggedin);
+    navbar = (
+      <nav className={styles.nav}>
+        <a href="/logout" className={navStyle}>
+          <span className={styles.nav__item}>Logout</span>
+        </a>
+      </nav>
+    );
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles["nav-container"]}>
         <div className={styles.header__logo}>
           <span className={styles.header__name}>Logo</span>
         </div>
-        <nav className={styles["nav"]}>
-          <a
-            onClick={navClickHandler}
-            href="/home"
-            className={props.home ? navActiveStyle : navStyle}
-          >
-            <span className={styles.nav__item}>Home</span>
-          </a>
-
-          <a
-            onClick={navClickHandler}
-            href="/login"
-            className={props.login ? navActiveStyle : navStyle}
-          >
-            <span className={styles.nav__item}>Login</span>
-          </a>
-
-          <a
-            onClick={navClickHandler}
-            href="/register"
-            className={props.register ? navActiveStyle : navStyle}
-          >
-            <span className={styles.nav__item}>Register</span>
-          </a>
-        </nav>
+        {navbar}
       </div>
     </header>
   );
