c433a78e49ec614ee742157ad6c91a6b36263b82
NeelMungra05 <nmungra@deloitte.com>
2023-06-11 17:53:38 +0530

    Separated out the code functionality
    -> Took out the card code i.e <div> with className of card from
    Services.jsx
    -> Transfered that part to ServiceList.jsx to create separate component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,52 +1,29 @@
 import React from "react";
 import styles from "./Services.module.css";
 import img from "../../assets/recon.jpg";
+import ServiceList from "./ServiceList";
 
 const Services = () => {
   return (
     <section className={styles.section}>
-      <div className={styles.section__items}>
-        <div className={styles.card}>
-          <img src={img} alt="Reconciliation" className={styles.card__img} />
-          <div className={styles.card__content}>
-            <h1 className={styles.card__header}>Data Reconciliation</h1>
-            <p className={styles.card__text}>
-              Get your data check and covered with our data reconciliation tool
-            </p>
-            <button className={styles.card__btn}>
-              Explore <span>&rarr;</span>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className={styles.section__items}>
-        <div className={styles.card}>
-          <img src={img} alt="Reconciliation" className={styles.card__img} />
-          <div className={styles.card__content}>
-            <h1 className={styles.card__header}>Data Reconciliation</h1>
-            <p className={styles.card__text}>
-              Get your data check and covered with our data reconciliation tool
-            </p>
-            <button className={styles.card__btn}>
-              Explore <span>&rarr;</span>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className={styles.section__items}>
-        <div className={styles.card}>
-          <img src={img} alt="Reconciliation" className={styles.card__img} />
-          <div className={styles.card__content}>
-            <h1 className={styles.card__header}>Data Reconciliation</h1>
-            <p className={styles.card__text}>
-              Get your data check and covered with our data reconciliation tool
-            </p>
-            <button className={styles.card__btn}>
-              Explore <span>&rarr;</span>
-            </button>
-          </div>
-        </div>
-      </div>
+      <ServiceList
+        img={img}
+        alt="Reconciliation"
+        header="Data Reconciliation"
+        content="Get your data check and covered with our data reconciliation tool"
+      />
+      <ServiceList
+        img={img}
+        alt="Reconciliation"
+        header="Data Reconciliation"
+        content="Get your data check and covered with our data reconciliation tool"
+      />
+      <ServiceList
+        img={img}
+        alt="Reconciliation"
+        header="Data Reconciliation"
+        content="Get your data check and covered with our data reconciliation tool"
+      />
     </section>
   );
 };
