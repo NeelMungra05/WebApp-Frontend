e49f963ee47e3727c8ce99f4f06a0ddae799d6d0
NeelMungra05 <nmungra@deloitte.com>
2023-06-14 16:54:08 +0530

    Styled the file input component

--- src/components/FileUpload/FileUpload.jsx
+++ src/components/FileUpload/FileUpload.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { AiOutlineFileText } from "react-icons/ai";
+import { BsChevronDown, BsCloudArrowUp } from "react-icons/bs";
+import FileList from "../FileList/FileList";
 import Input from "../UI/Input";
 import styles from "./FileUpload.module.css";
-import FileList from "../FileList/FileList";
 
 const FileUpload = (props) => {
   const { files } = props;
@@ -13,19 +15,28 @@ const FileUpload = (props) => {
 
   return (
     <>
-      <div className={styles.section__header}>Step 1</div>
-      <div className={styles.section__input}>
-        <Input
-          label={props.label}
-          input={{
-            type: "file",
-            multiple: true,
-            onChange: fileChangeHandler,
-            accept: props.accept,
-          }}
-        />
-        {files && <FileList files={files} onRemove={props.onRemove} />}
+      <div className={styles.uploadBox}>
+        <div className={`${styles.uploadBox__input} `}>
+          <BsCloudArrowUp fontSize={64} />
+          <Input
+            label={props.label}
+            input={{
+              type: "file",
+              multiple: true,
+              onChange: fileChangeHandler,
+              accept: props.accept,
+              className: styles["uploadBox__input-box"],
+            }}
+          />
+          <button type="button" className={styles.uploadBox__button}>
+            <AiOutlineFileText fontSize={20} />
+            <span>Choose files</span>
+            <BsChevronDown fontSize={20} />
+          </button>
+        </div>
       </div>
+      <div className={styles.section__header}>Step 1</div>
+      {files && <FileList files={files} onRemove={props.onRemove} />}
     </>
   );
 };
