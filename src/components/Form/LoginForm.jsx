f1844e053e7000071e22b37dc28c11339a93c11a
Faizal Kureshi <fkureshi@deloitte.com>
2023-06-14 17:53:32 +0530

    Styling Completed for login/register

--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -1,62 +1,66 @@
 import React from "react";
 import { useRef } from "react";
 import Input from "../UI/Input";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser,faKey } from '@fortawesome/free-solid-svg-icons'
-import './Form.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser,faKey } from '@fortawesome/free-solid-svg-icons';
+import styles from "./Form.module.css";
 
 const LoginForm = (props) => {
   const usernameInputRef = useRef();
   const passwordInputRef = useRef();
 
-  const loginButtonHandler = (e) => {
-    e.preventDefault();
+const loginButtonHandler = (e) => {
+  e.preventDefault();
 
-    const username = usernameInputRef.current.value;
-    const password = passwordInputRef.current.value;
+  const username = usernameInputRef.current.value;
+  const password = passwordInputRef.current.value;
 
-    props.loginHandler({
-      username,
-      password,
+  props.loginHandler({
+    username,
+    password,
     });
   };
+const registerHandler = (e) => {
+  e.preventDefault();
+  props.navbarStateChanger("register");
+};
 
   return (
   <>
     <section>
-      <div className="form-box-login">
-        <div className="form-value">
+      <div className={styles["form-box-login"]}>
+        <div className={styles["form-value"]}>
           <form action="">
             <h2>Login</h2>
-            <div className="inputbox">
-            <FontAwesomeIcon className="font-awesome" icon={faUser} />
+            <div className={styles.inputbox}>
+            <FontAwesomeIcon className={styles["font-awesome"]} icon={faUser} />
             <Input
-              className='input'
-              label={<label className="label">Username</label>}
+              label="Username"
+              reverse={true}
               ref={usernameInputRef}
               input={{
-              id: "username",
-              type: "text",
-              placeholder: " ",
-              }}/>
+                id: "username",
+                type: "text",
+                placeholder: " ",
+                    }}/>
             </div>
-            <div className="inputbox">
-            <FontAwesomeIcon className="font-awesome" icon={faKey} />
+            <div className={styles.inputbox}>
+            <FontAwesomeIcon className={styles["font-awesome"]} icon={faKey} />
               <Input
-              className='input'
-              label={<label className="label">Password</label>}
+              reverse={true}
+              label="Password"
               ref={passwordInputRef}
               input={{
-              id: "password",
-              type: "password",
-              placeholder: " ",
-              }}/>
+                id: "password",
+                type: "password",
+                placeholder: " ",
+                    }}/>
             </div>
             <button onClick={loginButtonHandler} type="submit">
               Login
             </button>
-            <div className="linkButton">
-              <p>Don't have a account? <a href="">Register</a></p>
+            <div className={styles.linkButton}>
+              <p>Don't have a account? <a onClick={registerHandler} href="/register" >Register</a></p>
             </div>
           </form>
         </div>
--- src/components/Form/SignupForm.jsx
+++ src/components/Form/SignupForm.jsx
@@ -3,7 +3,7 @@ import { useRef } from "react";
 import Input from "../UI/Input";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope,faUser,faKey,faCheck } from '@fortawesome/free-solid-svg-icons'
-// import "./Form.css"
+import styles from "./Form.module.css";
 
 const SignupForm = (props) => {
   const usernameInputRef = useRef();
@@ -11,70 +11,87 @@ const SignupForm = (props) => {
   const passwordInputRef = useRef();
   const confirmPasswordInputRef = useRef();
 
-  const formSubmitHandler = (e) => {
-    e.preventDefault();
+const formSubmitHandler = (e) => {
+  e.preventDefault();
 
-    const username = usernameInputRef.current.value;
-    const password = passwordInputRef.current.value;
-    const email = emailInputRef.current.value;
-    const confirmPassword = confirmPasswordInputRef.current.value;
+  const username = usernameInputRef.current.value;
+  const password = passwordInputRef.current.value;
+  const email = emailInputRef.current.value;
+  const confirmPassword = confirmPasswordInputRef.current.value;
 
-    props.registerHandler({
-      username,
-      email,
-      password,
-      confirmPassword,
+  props.registerHandler({
+    username,
+    email,
+    password,
+    confirmPassword,
     });
   };
+
+const loginHandler = (e) =>{
+  e.preventDefault();
+  props.navbarStateChanger("login");
+};
 
   return (
     <>
       <section>
-        <div className="form-box-register">
-          <div className="form-value">
+        <div className={styles["form-box-register"]}>
+          <div className={styles["form-value"]}>
             <form action="">
               <h2>Register</h2>
-              <div className="inputbox">
-              <FontAwesomeIcon className="font-awesome" icon={faEnvelope} />
-                <input
-                  placeholder=" "
-                  type="email"
+              <div className={styles["inputbox"]}>
+              <FontAwesomeIcon className={styles["font-awesome"]} icon={faEnvelope} />
+                <Input
+                  label="Email"
+                  reverse={true}
                   ref={emailInputRef}
-                />
-                  <label for="">Email</label>
+                  input={{
+                    id: "email",
+                    type: "email",
+                    placeholder: " ",
+                        }}/>
               </div>
-              <div className="inputbox">
-              <FontAwesomeIcon className="font-awesome" icon={faUser} />
-                <input
-                  placeholder=" "
-                  type="text"
-                  ref={usernameInputRef}
-                />
-                  <label for="">Username</label>
+              <div className={styles["inputbox"]}>
+              <FontAwesomeIcon className={styles["font-awesome"]} icon={faUser} />
+              <Input
+                label="Username"
+                reverse={true}
+                ref={usernameInputRef}
+                input={{
+                  id: "username",
+                  type: "text",
+                  placeholder: " ",
+                      }}/>
               </div>
-              <div className="inputbox">
-              <FontAwesomeIcon className="font-awesome" icon={faKey} />
-                <input
-                  placeholder=" "
-                  type="password"
-                  ref={passwordInputRef}
-                />
-                  <label for="">Password</label>
+              <div className={styles.inputbox}>
+              <FontAwesomeIcon className={styles["font-awesome"]} icon={faKey} />
+              <Input
+                label="Password"
+                reverse={true}
+                ref={passwordInputRef}
+                input={{
+                  id: "password",
+                  type: "password",
+                  placeholder: " ",
+                      }}/>
               </div>
-              <div className="inputbox">
-              <FontAwesomeIcon className="font-awesome" icon={faCheck} />
-                <input
-                  placeholder=" "
-                  type="password"
-                  ref={confirmPasswordInputRef}
-                />
-                  <label for="">Confirm Password</label>
+              <div className={styles["inputbox"]}>
+              <FontAwesomeIcon className={styles["font-awesome"]} icon={faCheck} />
+              <Input
+                label="Confirm Password"
+                reverse={true}
+                ref={confirmPasswordInputRef}
+                input={{
+                  id: "password",
+                  type: "password",
+                  placeholder: " ",
+                      }}/>
               </div>
               <button onClick={formSubmitHandler} type="submit">
                 Register
               </button>
-              <div className="linkButton">
-              <p>Already have an account? <a href="">Login</a></p>
+              <div className={styles.linkButton}>
+              <p>Already have an account? <a onClick={loginHandler} href="/login">Login</a></p>
               </div>
             </form>
           </div>
