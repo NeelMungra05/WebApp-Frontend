70d35155a25be06b2fac6d7d9d7d763e648dfad1
NeelMungra05 <nmungra@deloitte.com>
2023-06-14 12:21:43 +0530

    Added FileUpload component for Source & Target

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import FileUpload from "../FileUpload/FileUpload";
 
 const Upload = () => {
-  return <div>Upload</div>;
+  return (
+    <div>
+      <FileUpload />
+      <FileUpload />
+    </div>
+  );
 };
 
 export default Upload;
