3da19c86cf27b241ebf4eb3951416b6c19f3d0ec
NeelMungra05 <nmungra@deloitte.com>
2023-06-15 17:36:17 +0530

    Added steps info header
    -> Used useSteps hook to get length and steps header
    -> Used that to display and used steps from Reducer to progress bar
    -> Styled the steps header display

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import styles from "./ProgressBar.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import useSteps from "../../hooks/useSteps";
 
 const ProgressBar = () => {
+  const steps = useSelector((state) => state.steps.steps);
+
+  const { length, stepInfoArr: stepArr } = useSteps();
+
+  const fillProgression = (steps / length) * 100;
   const barFillStyle = {
-    width: `30%`,
+    width: `${fillProgression}%`,
   };
+
+  const stepsInfo = stepArr.map((data, idx) => {
+    return (
+      <p>
+        <span>{idx + 1}</span>
+        <span>{data}</span>
+      </p>
+    );
+  });
 
   return (
     <div className={styles.container}>
+      <div className={styles.steps}>{stepsInfo}</div>
       <div className={styles.progress_bar}>
         <div className={styles.progress_bar__fill} style={barFillStyle}></div>
       </div>
