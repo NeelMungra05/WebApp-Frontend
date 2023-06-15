47aa0497c2ae41e712629f9b5c1738e61fea6fc7
NeelMungra05 <nmungra@deloitte.com>
2023-06-15 16:56:38 +0530

    Added custom hook to store constant steps

---
+++ src/hooks/useSteps.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const STEPS_INFO = {
+  0: "Src & Target Upload",
+  1: "Field Selection",
+  2: "Src & Target Mapping",
+};
+
+const useSteps = () => {
+  return STEPS_INFO;
+};
+
+export default useSteps;
