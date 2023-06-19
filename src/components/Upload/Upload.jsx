32ce7d6a26d1b517b7684ad74c0945cc0107ecaa
NeelMungra05 <nmungra@deloitte.com>
2023-06-19 13:49:41 +0530

    Added styling to Upload component

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import FileUpload from "../FileUpload/FileUpload";
+import styles from "./Upload.module.css";
 
 const Upload = () => {
   return (
-    <div>
-      <FileUpload lable=" " accept=".xlsx" heading="Target Files Upload" />
-      <FileUpload label=" " accept=".xlsx" heading="Source Files Upload" />
+    <div className={styles.uploader}>
+      <div className={styles.uploader__source}>
+        <FileUpload label=" " accept=".xlsx" heading="Source Files Upload" />
+      </div>
+      <div className={styles.uploader__target}>
+        <FileUpload lable=" " accept=".xlsx" heading="Target Files Upload" />
+      </div>
     </div>
   );
 };
