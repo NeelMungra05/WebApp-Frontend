9345f4eedcff8deba56a5b0fc6103ccebd45dea2
NeelMungra05 <nmungra@deloitte.com>
2023-06-19 11:19:08 +0530

    Fix the visual bug
    -> Changed the styling part in Form.module.css
    -> Where it was breaking the whole application
    -> Cause:- directly applying the styling to html tag
    -> Solution:- made that element to class

--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { useRef } from "react";
 import Input from "../UI/Input";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faKey } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Form.module.css";
 
 const LoginForm = (props) => {
   const usernameInputRef = useRef();
@@ -17,33 +20,65 @@ const LoginForm = (props) => {
       password,
     });
   };
+  const registerHandler = (e) => {
+    e.preventDefault();
+    props.navbarStateChanger("register");
+  };
 
   return (
     <>
-      <form action="">
-        <Input
-          label="Username"
-          ref={usernameInputRef}
-          input={{
-            id: "username",
-            type: "text",
-          }}
-        />
-        <Input
-          label="Password"
-          ref={passwordInputRef}
-          input={{
-            id: "password",
-            type: "password",
-          }}
-        />
-        <button onClick={loginButtonHandler} type="submit">
-          Login
-        </button>
-      </form>
-      <p>
-        Not yet Registered ! <a href="">Register here</a>
-      </p>
+      <section className={styles.section}>
+        <div className={styles["form-box-login"]}>
+          <div className={styles["form-value"]}>
+            <form action="">
+              <h2>Login</h2>
+              <div className={styles.inputbox}>
+                <FontAwesomeIcon
+                  className={styles["font-awesome"]}
+                  icon={faUser}
+                />
+                <Input
+                  label="Username"
+                  reverse={true}
+                  ref={usernameInputRef}
+                  input={{
+                    id: "username",
+                    type: "text",
+                    placeholder: " ",
+                  }}
+                />
+              </div>
+              <div className={styles.inputbox}>
+                <FontAwesomeIcon
+                  className={styles["font-awesome"]}
+                  icon={faKey}
+                />
+                <Input
+                  reverse={true}
+                  label="Password"
+                  ref={passwordInputRef}
+                  input={{
+                    id: "password",
+                    type: "password",
+                    placeholder: " ",
+                  }}
+                />
+              </div>
+              <button onClick={loginButtonHandler} type="submit">
+                Login
+              </button>
+              <div className={styles.linkButton}>
+                <p>
+                  Don't have a account?{" "}
+                  <a onClick={registerHandler} href="/register">
+                    Register
+                  </a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
