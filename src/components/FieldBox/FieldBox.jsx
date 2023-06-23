59423447d9f20c0eec27f8bab13e2d9389dbc27c
NeelMungra05 <nmungra@deloitte.com>
2023-06-23 16:31:07 +0530

    Added intermediate styling for the field box

--- src/components/FieldBox/FieldBox.jsx
+++ src/components/FieldBox/FieldBox.jsx
@@ -1,14 +1,15 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { useRef } from "react";
 import Input from "../UI/Input";
 import styles from "./FieldBox.module.css";
 
 const FieldBox = () => {
   return (
-    <div>
-      <h6>Filename.xlsx</h6>
-      <div>
+    <div className={styles.container}>
+      <h6 className={styles.heading}>Filename.xlsx</h6>
+      <div className={styles.container__search}>
         <Input
           label=""
           input={{
@@ -16,9 +17,12 @@ const FieldBox = () => {
             placeholder: "Search the fieldname",
           }}
         />
-        <FontAwesomeIcon icon={faMagnifyingGlass} />
+        <FontAwesomeIcon
+          icon={faMagnifyingGlass}
+          className={styles["container__search--logo"]}
+        />
       </div>
-      <div>
+      <div className={styles.container__fields}>
         <Input
           label="field 1"
           reverse={true}
@@ -26,6 +30,19 @@ const FieldBox = () => {
             type: "checkbox",
           }}
         />
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+          doloremque inventore adipisci vero perspiciatis voluptate, qui aut
+          velit, veritatis fuga sit? Dolorum aliquam nihil illum quam quia nemo
+          delectus architecto, dolorem quae inventore corporis omnis vel, quidem
+          ullam. Accusantium officia repudiandae voluptas! Aut quos
+          exercitationem et labore debitis, voluptate facilis, unde ex nisi
+          corporis quam vitae? Ut, porro. Numquam aliquid tempore sit? Saepe
+          provident impedit doloremque error qui ut a atque illum perferendis
+          quasi! Nulla sed repellendus ad, eligendi delectus, nemo sunt quasi
+          deserunt, aliquam nesciunt quidem modi? Eum itaque quidem possimus?
+          Minus animi eligendi at esse sint accusamus maiores.
+        </div>
       </div>
     </div>
   );
