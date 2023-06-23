3897a693795dff7137d243985454d3454f2678eb
NeelMungra05 <nmungra@deloitte.com>
2023-06-23 10:48:09 +0530

    Added the FieldBox component in FieldSelector
    -> Added base code to FieldBox

--- src/components/FieldBox/FieldBox.jsx
+++ src/components/FieldBox/FieldBox.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Input from "../UI/Input";
+import styles from "./FieldBox.module.css";
 
 const FieldBox = () => {
-  return <div>FieldBox</div>;
+  return (
+    <div>
+      <h6>Filename.xlsx</h6>
+      <Input
+        label=""
+        input={{
+          type: "text",
+          placeholder: "Search the fieldname",
+        }}
+      />
+      <div>
+        <Input
+          label="field 1"
+          reverse={true}
+          input={{
+            type: "checkbox",
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FieldBox;
--- src/components/FieldSelector/FieldSelector.jsx
+++ src/components/FieldSelector/FieldSelector.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import FieldBox from "../FieldBox/FieldBox";
+import styles from "./FieldSelector.module.css";
 
 const FieldSelector = () => {
   return (
     <article>
       <h3>Source Field Selection</h3>
+      <div style={styles.container}>
+        <FieldBox />
+        <FieldBox />
+        <FieldBox />
+      </div>
     </article>
   );
 };
