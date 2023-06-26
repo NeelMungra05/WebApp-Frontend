672d0a0335b476dabaabb23bc8d7b348c0315a52
NeelMungra05 <nmungra@deloitte.com>
2023-06-26 14:50:17 +0530

    Added dynamic props to take input
    -> Added propeties to add filename and render fields available in
    FieldBox

--- src/components/FieldBox/FieldBox.jsx
+++ src/components/FieldBox/FieldBox.jsx
@@ -5,10 +5,25 @@ import { useRef } from "react";
 import Input from "../UI/Input";
 import styles from "./FieldBox.module.css";
 
-const FieldBox = () => {
+const FieldBox = (props) => {
+  const { fileName, fields, id } = props;
+
+  const fieldsChoice = fields.map((field) => (
+    <Input
+      label={field}
+      reverse={true}
+      input={{
+        key: `${id}${fileName}${field}`,
+        id: `${id}${fileName}${field}`,
+        name: `${fileName}-${field}`,
+        type: "checkbox",
+      }}
+    />
+  ));
+
   return (
     <div className={styles.container}>
-      <h6 className={styles.heading}>Filename.xlsx</h6>
+      <h6 className={styles.heading}>{fileName}</h6>
       <div className={styles.container__search}>
         <Input
           label=""
@@ -22,31 +37,7 @@ const FieldBox = () => {
           className={styles["container__search--logo"]}
         />
       </div>
-      <div className={styles.container__fields}>
-        <Input
-          label="field 1"
-          reverse={true}
-          input={{
-            id: "field1",
-            type: "checkbox",
-          }}
-        />
-        <Input
-          label="field 2"
-          reverse={true}
-          input={{
-            id: "field2",
-            type: "checkbox",
-          }} />
-        <div>
-        Here you will find our list of known bugs and pending feature
-        requests. If your problem is not listed in there, or in the FAQ, or
-        in the manuals, read the Feedback page to find out how to report
-        bugs to us. PLEASE read the Feedback page carefully: it is there to
-        save you time as well as us. Do not send us one-line bug reports
-        telling us `it doesn't work'.
-        </div>
-      </div>
+      <div className={styles.container__fields}>{fieldsChoice}</div>
     </div>
   );
 };
--- src/components/FieldSelector/FieldSelector.jsx
+++ src/components/FieldSelector/FieldSelector.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import FieldBox from "../FieldBox/FieldBox";
 import styles from "./FieldSelector.module.css";
+import { useSelector } from "react-redux";
 
 const FieldSelector = () => {
+  const sourceField = useSelector((state) => state.fields.sourceFields);
+
+  const content = sourceField.map((data, idx) => (
+    <FieldBox
+      fileName={data.name}
+      fields={data.result}
+      id={idx}
+      key={`${idx}${data.name}`}
+    />
+  ));
+
   return (
     <article>
       <h3>Source Field Selection</h3>
-      <div className={styles.container}>
-        <FieldBox />
-        <FieldBox />
-        <FieldBox />
-        <FieldBox />
-        <FieldBox />
-      </div>
+      <div className={styles.container}>{content}</div>
     </article>
   );
 };
