5117a1b0cc6562cf7f0a2aba2393b351fb3969c0
NeelMungra05 <nmungra@deloitte.com>
2023-07-04 11:07:08 +0530

    Added reducer logic for file upload validation

--- src/store/validations.jsx
+++ src/store/validations.jsx
@@ -14,5 +14,25 @@ const initialState = {
       areLessThan50MB: true,
       isValidFileFormat: true,
     },
+    isValid: false,
   },
 };
+
+const validationSlice = createSlice({
+  initialState,
+  name: "validation",
+  reducers: {
+    changeUploadFileValidation(state, action) {
+      state.fileUpload.source = action.payload.source;
+      state.fileUpload.target = action.payload.target;
+      state.fileUpload.isValid = Object.values(
+        action.payload.source && action.payload.target
+      ).reduce(prev, (curr) => prev && curr);
+    },
+  },
+});
+
+const validationReducer = validationSlice.reducer;
+
+export const validationAction = validationSlice.actions;
+export default validationReducer;
