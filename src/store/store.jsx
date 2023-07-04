8949fe28647e41349e85f0fe5eb5d49993d02d40
NeelMungra05 <nmungra@deloitte.com>
2023-07-04 11:07:54 +0530

    Added validationReducer to redux store

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -3,6 +3,7 @@ import stepsReducer from "./steps";
 import formButtonReducer from "./formButton";
 import fileReducer from "./files";
 import fieldsReducer from "./fields-slice";
+import validationReducer from "./validations";
 
 const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
     formButton: formButtonReducer,
     files: fileReducer,
     fields: fieldsReducer,
+    validations: validationReducer,
   },
 });
 
