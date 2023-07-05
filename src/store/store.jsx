f8d33c981a52f70811d7026eacbaf3fe513c729a
NeelMungra05 <nmungra@deloitte.com>
2023-07-05 15:05:33 +0530

    Removed validation from redux state
    -> Found it will only be used per particular page
    -> Will not be required elsewhere
    -> So only custom hook will suffice the requirements

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -3,7 +3,6 @@ import stepsReducer from "./steps";
 import formButtonReducer from "./formButton";
 import fileReducer from "./files";
 import fieldsReducer from "./fields-slice";
-import validationReducer from "./validations";
 
 const store = configureStore({
   reducer: {
@@ -11,7 +10,6 @@ const store = configureStore({
     formButton: formButtonReducer,
     files: fileReducer,
     fields: fieldsReducer,
-    validation: validationReducer,
   },
 });
 
