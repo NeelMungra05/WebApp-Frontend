8bd7d9c23c091464da7728cdea7eea9e2b48c9de
NeelMungra05 <nmungra@deloitte.com>
2023-07-05 19:02:02 +0530

    Added validation for fileUpload
    -> Added validation hook for fileUpload covering the file length file
    size
    -> The hook will provide the sanity check on this parameter
    -> Later custom error message can be shown on the basis of sanity passed

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import useFileUploadValidation from "../../hooks/useFileUploadValidation";
 import FileUpload from "../FileUpload/FileUpload";
 import styles from "./Upload.module.css";
+import { formButtonAction } from "../../store/formButton";
 
 const Upload = () => {
+  const sourceValidation = useFileUploadValidation({ type: "source" });
+  const targetValidation = useFileUploadValidation({ type: "target" });
+  const dispatch = useDispatch();
+
+  const isValid = Object.values(sourceValidation).every(
+    (condition, idx) =>
+      condition === true && Object.values(targetValidation)[idx] === condition
+  );
+
+  console.table(sourceValidation && targetValidation);
+  dispatch(formButtonAction.nextButton(isValid));
+
   return (
     <div className={styles.uploader}>
       <div className={styles.uploader__source}>
+        {!sourceValidation.isValidFileFormat && (
+          <p className={styles.error}>File should be in xlsx format only.</p>
+        )}
         <FileUpload label=" " accept=".xlsx" heading="Source File Upload" />
       </div>
 
