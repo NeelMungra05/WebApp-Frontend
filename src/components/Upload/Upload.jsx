d999d8646873c16c66c864cb43adaf50d880ad04
NeelMungra05 <nmungra@deloitte.com>
2023-07-05 10:38:21 +0530

    Beautify the code

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -5,22 +5,15 @@ import styles from "./Upload.module.css";
 const Upload = () => {
   return (
     <div className={styles.uploader}>
-     
       <div className={styles.uploader__source}>
-   
-        <FileUpload 
-        label=" " 
-        accept=".xlsx" 
-        heading ="Source File Upload"
-         />
+        <FileUpload label=" " accept=".xlsx" heading="Source File Upload" />
       </div>
-      
+
       <div className={styles.uploader__target}>
-     
         <FileUpload
           label=" "
           accept=".xlsx"
-          heading = "Target File Upload"
+          heading="Target File Upload"
           isSource={false}
         />
       </div>
