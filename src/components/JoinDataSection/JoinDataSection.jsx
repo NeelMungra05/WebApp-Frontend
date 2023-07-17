8f381f072a9b9f17a68a90bca87eec1f0a8cf627
MihikaBodke19 <mbodke@deloitte.com>
2023-07-17 12:05:19 +0530

    "Join Data changes"

--- src/components/JoinDataSection/JoinDataSection.jsx
+++ src/components/JoinDataSection/JoinDataSection.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../JoinDataSection/JoinDataSection.module.css";
+// import { IconName } from "react-icons/fa";
+
 
 const JoinDataSection =({ join, index, onDiscard }) => {
   return (
@@ -17,13 +19,28 @@ const JoinDataSection =({ join, index, onDiscard }) => {
 
 <div className = {styles.joindata}>
     
-        {join.tables.map((table, tableIndex) => (
-          <select className={styles.joinselect}>
+        {/* {join.tables.map((table, tableIndex) => (
+          <select className={styles.joinselect} >
           <option key={tableIndex}>{table}</option>
-          <option key={tableIndex}>{table}</option>
-          
           </select>
-        ))}
+    ))} */}
+
+
+  {join.tables.map((table, tableIndex) => (
+          <select id="multiple-select" multiple>
+          <option value="1">Books</option>
+          <option value="2">Movies, Music & Games</option>
+          <option value="3">Electronics & Computers</option>
+          <option value="4">Home, Garden & Tools</option>
+          <option value="5">Health & Beauty</option>
+          <option value="6">Toys, Kids & Baby</option>
+          <option value="7">Clothing & Jewelry</option>
+          <option value="8">Sports & Outdoors</option>
+      </select>
+    ))}
+
+
+
         </div>
         
       
