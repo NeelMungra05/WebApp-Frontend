43fe56ad4fc4a5d1463797590432fd8c2953cbaf
Faizal Kureshi <fkureshi@deloitte.com>
2023-07-18 19:08:22 +0530

    styling on 3rd screen updated

---
+++ src/components/svgIcons/InnerjoinIcon.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const InnerjoinIcon = () => {
+    return(
+        <svg
+            version="1.1"
+            width="1080"
+            height="1080"
+            viewBox="0 0 1080 1080"
+            xmlSpace="preserve"
+        >            <desc>Created with Fabric.js 5.2.4</desc>
+            <defs>
+            </defs>
+            <g transform="matrix(1 0 0 1 540 540)" id="32d23dd6-e068-4adb-b947-affad057a374"  >
+            <rect style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255,255,255); fill-rule: nonzero; opacity: 1; visibility: hidden;" vector-effect="non-scaling-stroke"  x="-540" y="-540" rx="0" ry="0" width="1080" height="1080" />
+            </g>
+            <g transform="matrix(1 0 0 1 540 540)" id="ce09baba-37f7-4ef5-81c5-5e6a93f489b9"  >
+            </g>
+            <g transform="matrix(1 0 0 1 424.2 539.98)"  >
+            <path style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(78,148,79); fill-rule: evenodd; opacity: 0.99;" vector-effect="non-scaling-stroke"  transform=" translate(-139.65, -255.48)" d="M 254.5 156.5 C 254.833 157.167 255.167 157.833 255.5 158.5 C 222.114 192.836 209.28 233.836 217 281.5 C 222.572 309.324 235.405 332.991 255.5 352.5 C 255.167 353.167 254.833 353.833 254.5 354.5 C 210.31 393.08 160.644 401.913 105.5 381 C 48.4018 352.66 21.2351 306.494 24 242.5 C 32.2222 182.562 65.0555 143.062 122.5 124 C 172.482 111.421 216.482 122.255 254.5 156.5 Z" stroke-linecap="round" />
+            </g>
+            <g transform="matrix(1 0 0 1 655.35 540)"  >
+            <path style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(78,148,79); fill-rule: evenodd; opacity: 1;" vector-effect="non-scaling-stroke"  transform=" translate(-370.8, -255.5)" d="M 254.5 354.5 C 254.833 353.833 255.167 353.167 255.5 352.5 C 290.838 315.501 303.005 271.835 292 221.5 C 285.448 197.038 273.281 176.038 255.5 158.5 C 255.167 157.833 254.833 157.167 254.5 156.5 C 255.552 156.649 256.552 156.483 257.5 156 C 288.813 127.008 325.813 115.008 368.5 120 C 426.805 129.971 464.972 163.137 483 219.5 C 496.668 284.626 476.168 335.792 421.5 373 C 368.819 400.905 317.819 398.238 268.5 365 C 264.534 362.036 260.867 358.703 257.5 355 C 256.552 354.517 255.552 354.351 254.5 354.5 Z" stroke-linecap="round" />
+            </g>
+            <g transform="matrix(1 0 0 1 539.92 540)"  >
+            <path style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(134,188,37); fill-rule: evenodd; opacity: 1;" vector-effect="non-scaling-stroke"  transform=" translate(-255.37, -255.5)" d="M 255.5 158.5 C 273.281 176.038 285.448 197.038 292 221.5 C 303.005 271.835 290.838 315.501 255.5 352.5 C 235.405 332.991 222.572 309.324 217 281.5 C 209.28 233.836 222.114 192.836 255.5 158.5 Z" stroke-linecap="round" />
+            </g>
+        </svg>
+    )
+    };
+
+export default InnerjoinIcon;
