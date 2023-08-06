cd4883659041abd70300ef66bd6540c567328a0c
NeelMungra05 <nmungra@deloitte.com>
2023-08-06 10:08:00 +0530

    Changed the steps names

--- src/hooks/useSteps.jsx
+++ src/hooks/useSteps.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 const STEPS_INFO = {
-  0: "Src & Trgt Upload",
+  0: "Data Object Upload",
   1: "Field Selection",
-  2: "Src & Trgt Join",
-  3: "Src & Trgt Mapping",
-  4: "RF Selection",
+  2: "Joining Condition",
+  3: "Primary Key Condition",
+  4: "Recon Order",
   5: "Summary",
 };
 
