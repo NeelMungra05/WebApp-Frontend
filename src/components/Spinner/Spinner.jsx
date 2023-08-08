c2aaa37bf67aad5159b7fd1c5edff6b456d7e0c9
NeelMungra05 <nmungra@deloitte.com>
2023-08-08 14:19:17 +0530

    Added new component spinner

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Spinner.module.css";
+
+const Spinner = () => {
+  return (
+    <div className={styles.example}>
+      <span className={styles.spinner} />
+    </div>
+  );
+};
+
+export default Spinner;
