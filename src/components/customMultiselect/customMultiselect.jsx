6099953da16df46d40e77796f7b7e1c8afabc22e
Faizal Kureshi <fkureshi@deloitte.com>
2023-09-15 16:37:17 +0530

    added select all option in dropdown

--- src/components/customMultiselect/customMultiselect.jsx
+++ src/components/customMultiselect/customMultiselect.jsx
@@ -1,30 +1,38 @@
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../customMultiselect/customMultiselect.module.css";
 
 const CustomMultiselect = ({ options, placeholder, onAddFields, isLeft }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectAllEnabled, setSelectAllEnabled] = useState(options.length > 0);
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
 
   const handleOptionClick = (option) => {
-    const isSelected = selectedOptions.includes(option);
-
-    setSelectedOptions((prevState) =>
-      isSelected
-        ? prevState.filter((item) => item !== option)
-        : [...prevState, option]
-    );
+    if (option === "Select All") {
+      if (selectedOptions.length === options.length) {
+        setSelectedOptions([]);
+      } else {
+        setSelectedOptions(options);
+      }
+    } else {
+      const isSelected = selectedOptions.includes(option);
+      setSelectedOptions((prevState) =>
+        isSelected
+          ? prevState.filter((item) => item !== option)
+          : [...prevState, option]
+      );
+    }
   };
 
   useEffect(() => {
     if (onAddFields && isLeft !== undefined) {
       onAddFields(isLeft, selectedOptions);
     }
-  }, [selectedOptions]);
+    setSelectAllEnabled(options.length > 0);
+  }, [selectedOptions, options, onAddFields, isLeft]);
 
   return (
     <div className={styles.dropdown}>
@@ -42,8 +50,7 @@ const CustomMultiselect = ({ options, placeholder, onAddFields, isLeft }) => {
                     setSelectedOptions(
                       selectedOptions.filter((item) => item !== option)
                     )
-                  }
-                >
+                  }>
                   &times;
                 </span>
               </div>
@@ -54,6 +61,22 @@ const CustomMultiselect = ({ options, placeholder, onAddFields, isLeft }) => {
       </div>
       {isOpen && (
         <div className={styles.dropdown__content}>
+          {/* Add the "Select All" option */}
+          <div
+            className={
+              selectedOptions.length === options.length
+                ? styles.dropdown__optionSelected
+                : styles.dropdown__option
+            }
+            onClick={() => handleOptionClick("Select All")}
+            disabled={!selectAllEnabled}>
+            <input
+              type="checkbox"
+              checked={selectedOptions.length === options.length}
+              readOnly
+            />
+            <label>Select All</label>
+          </div>
           {options.map((option, index) => (
             <div
               key={index}
@@ -62,8 +85,7 @@ const CustomMultiselect = ({ options, placeholder, onAddFields, isLeft }) => {
                   ? styles.dropdown__optionSelected
                   : styles.dropdown__option
               }
-              onClick={() => handleOptionClick(option)}
-            >
+              onClick={() => handleOptionClick(option)}>
               <input
                 type="checkbox"
                 checked={selectedOptions.includes(option)}
