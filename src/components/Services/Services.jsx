db0842a202a9b2a167950f397f0749a619c74af7
NeelMungra05 <nmungra@deloitte.com>
2023-11-23 15:48:29 +0530

    Added logic to render the sub services available
    -> Removed all the logic part from Service.jsx and transferred to custom
    hook useServiceHandler.jsx
    -> Now added a dynamic way to render the all available services and sub
    services in a single component i.e. Service.jsx itself.
    -> Extracted out all the service and sub service data to
    useServiceHandler.jsx along with the state which manage what service and
    sub service has been selected

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,96 +1,38 @@
 import React from "react";
 import styles from "./Services.module.css";
-import img from "../../assets/recon.png";
-import img1 from "../../assets/profiling.png";
-import img2 from "../../assets/transformation.png";
 import ServiceList from "./ServiceList";
 import { useState } from "react";
 import { useMemo } from "react";
 import Reconciliation from "./Reconciliation/Reconciliation";
 import { Provider } from "react-redux";
 import { store } from "../../store/store";
+import useServiceHandler from "../../hooks/useServiceHandler";
 
 const Services = () => {
-  const [serviceSelected, setServiceSelected] = useState({
-    reconciliation: false,
-    profiling: false,
-    transformation: false,
-  });
+  const { MAIN_SERVICE, SUB_SERVICE, serviceSelected } = useServiceHandler();
 
-  const reconciliationHandler = (e) => {
-    setServiceSelected({
-      reconciliation: true,
-      profiling: false,
-      transformation: false,
-    });
+  const serviceListGenerator = (serviceData) => {
+    return serviceData.map((data) => (
+      <ServiceList
+        key={data.id}
+        img={data.img}
+        alt={data.alt}
+        header={data.header}
+        content={data.content}
+        onClick={data.onClick}
+      />
+    ));
   };
 
-  const profilingHandler = (e) => {
-    setServiceSelected({
-      reconciliation: false,
-      profiling: true,
-      transformation: false,
-    });
-  };
+  let service = serviceListGenerator(MAIN_SERVICE);
 
-  const transformationHandler = (e) => {
-    setServiceSelected({
-      reconciliation: false,
-      profiling: false,
-      transformation: true,
-    });
-  };
-
-  const SERVICE_INFO = useMemo(() => {
-    return [
-      {
-        id: 1,
-        img: img,
-        alt: "Reconciliation",
-        header: "Data Reconciliation",
-        content:
-          // "Comparing and aligning data from various sources to ensure accuracy, consistency, and reliable insights for effective decision-making.",
-          "Unlocking Data Precision: Introducing Our State-of-the-Art Reconciliation Tool for Streamlined Accuracy and Unparalleled Insights.",
-        onClick: reconciliationHandler,
-      },
-      {
-        id: 2,
-        img: img1,
-        alt: "Profiling",
-        header: "Data Profiling",
-        content:
-          "Mastering Data Excellence: Unveiling Our Dynamic Data Profiling & Quality Analysis Tool for Informed Decision-Making & Precision!",
-        onClick: profilingHandler,
-      },
-      {
-        id: 3,
-        img: img2,
-        alt: "Transformation",
-        header: "Data Transformation",
-        content:
-          "Align today's data with tomorrow's Process : Unleashing the Future of Data with Our Transformative Tool for Seamless Data Integration and Analysis.",
-        onClick: transformationHandler,
-      },
-    ];
-  }, [reconciliationHandler, profilingHandler, transformationHandler]);
-
-  let service = SERVICE_INFO.map((data) => (
-    <ServiceList
-      key={data.id}
-      img={data.img}
-      alt={data.alt}
-      header={data.header}
-      content={data.content}
-      onClick={data.onClick}
-    />
-  ));
-
-  if (serviceSelected.reconciliation) {
-    return (
-      <Provider store={store}>
-        <Reconciliation />
-      </Provider>
-    );
+  if (serviceSelected.reconciliation.selected) {
+    service = serviceListGenerator(SUB_SERVICE);
+    // return (
+    //   <Provider store={store}>
+    //     <Reconciliation />
+    //   </Provider>
+    // );
   }
 
   return <section className={styles.section}>{service}</section>;
--- src/hooks/useServiceHandler.jsx
+++ src/hooks/useServiceHandler.jsx
@@ -1,7 +1,102 @@
 import React from "react";
+import { useState, useMemo } from "react";
+import img from "../assets/recon.png";
+import img1 from "../assets/profiling.png";
+import img2 from "../assets/transformation.png";
 
 const useServiceHandler = () => {
-  return <div>useServiceHandler</div>;
+  const [serviceSelected, setServiceSelected] = useState({
+    reconciliation: {
+      selected: false,
+      type: { postLoad: false, financial: false },
+    },
+    profiling: false,
+    transformation: false,
+  });
+
+  /**
+   *
+   * @param {string} serviceType - The type of service selected
+   * @param {string} serviceModule - The name of subservice selected
+   */
+  const serviceHandler = (serviceType, serviceModule) => {
+    setServiceSelected({
+      reconciliation:
+        serviceType === "reconciliation"
+          ? {
+              selected: true,
+              type: {
+                postLoad: serviceModule === "postload",
+                financial: serviceModule === "financial",
+              },
+            }
+          : false,
+      profiling: serviceType === "profiling",
+      transformation: serviceType === "transformation",
+    });
+  };
+
+  const MAIN_SERVICE = useMemo(() => {
+    return [
+      {
+        id: 1,
+        img: img,
+        alt: "Reconciliation",
+        header: "Data Reconciliation",
+        content:
+          // "Comparing and aligning data from various sources to ensure accuracy, consistency, and reliable insights for effective decision-making.",
+          "Unlocking Data Precision: Introducing Our State-of-the-Art Reconciliation Tool for Streamlined Accuracy and Unparalleled Insights.",
+        onClick: () => serviceHandler("reconciliation"),
+      },
+      {
+        id: 2,
+        img: img1,
+        alt: "Profiling",
+        header: "Data Profiling",
+        content:
+          "Mastering Data Excellence: Unveiling Our Dynamic Data Profiling & Quality Analysis Tool for Informed Decision-Making & Precision!",
+        onClick: () => serviceHandler("profiling"),
+      },
+      {
+        id: 3,
+        img: img2,
+        alt: "Transformation",
+        header: "Data Transformation",
+        content:
+          "Align today's data with tomorrow's Process : Unleashing the Future of Data with Our Transformative Tool for Seamless Data Integration and Analysis.",
+        onClick: () => serviceHandler("transformation"),
+      },
+    ];
+  }, [serviceHandler]);
+
+  const SUB_SERVICE = useMemo(() => {
+    return [
+      {
+        id: 1,
+        img: img,
+        alt: "Preload Reconciliation",
+        header: "Preload Reconciliation",
+        content:
+          "Align today's data with tomorrow's Process : Unleashing the Future of Data with Our Transformative Tool for Seamless Data Integration and Analysis.",
+        onClick: () => serviceHandler("reconciliation", "postload"),
+      },
+      {
+        id: 2,
+        img: img,
+        alt: "Financial Reconciliation",
+        header: "Financial Reconciliation",
+        content:
+          "Align today's data with tomorrow's Process : Unleashing the Future of Data with Our Transformative Tool for Seamless Data Integration and Analysis.",
+        onClick: () => serviceHandler("reconciliation", "financial"),
+      },
+    ];
+  }, [serviceHandler]);
+
+  return {
+    MAIN_SERVICE,
+    SUB_SERVICE,
+    serviceSelected,
+  };
 };
 
 export default useServiceHandler;
