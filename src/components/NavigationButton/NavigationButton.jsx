af3706724e29b2f8df448dbaee8396f112c0931a
NeelMungra05 <nmungra@deloitte.com>
2023-12-07 10:16:15 +0530

    Decoupled Navigation buttons from Reconciliation
    -> Extracted out the Navigation button logic from Reconciliation and
    made a separate component for that NavigationButton.jsx
    -> Also extracted out the styling to separate css module.

--- src/components/NavigationButton/NavigationButton.jsx
+++ src/components/NavigationButton/NavigationButton.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "./NavigationButton.module.css";
 
-const NavigationButton = () => {
-  return <div>NavigationButton</div>;
+const NavigationButton = ({ handleButton, prevButton, nextButton }) => {
+  return (
+    <div className={styles.form__navigation}>
+      <button
+        type="button"
+        className={`${styles.form__button} ${styles.form__button__previous}`}
+        disabled={!prevButton}
+        onClick={() => handleButton("prev")}
+      >
+        <span className={styles["form__navigation--back"]}>Previous</span>
+      </button>
+      <button
+        type="button"
+        className={`${styles.form__button} ${styles.form__button__next}`}
+        disabled={!nextButton}
+        onClick={() => handleButton("next")}
+      >
+        <span className={styles["form__navigation--forward"]}>Next</span>
+      </button>
+    </div>
+  );
 };
 
 export default NavigationButton;
