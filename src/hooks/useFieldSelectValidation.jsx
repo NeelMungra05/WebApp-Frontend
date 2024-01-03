f8f2d71085a7461271136fe0b6d686979087a20b
Faizal Kureshi <fkureshi@deloitte.com>
2024-01-03 17:55:03 +0530

    Streamlined the useFieldSelectValidation

--- src/hooks/useFieldSelectValidation.jsx
+++ src/hooks/useFieldSelectValidation.jsx
@@ -1,29 +1,26 @@
-import { useMemo } from "react";
-import { useCallback } from "react";
 import { useSelector } from "react-redux";
 
 const useFieldSelectValidation = () => {
   const sourceFields = useSelector((state) => state.fields.sourceFields);
   const targetFields = useSelector((state) => state.fields.targetFields);
 
-  console.log(targetFields);
-
-  const hasSelectedFields = (fields, property) =>
+  const createValidation = (fields, property) =>
     Object.keys(fields).every((key) =>
       Object.values(fields[key]).some((val) => val[property] === true)
     );
 
-  const hasSelectedSourceRF = hasSelectedFields(sourceFields, "RF");
-  const hasSelectedTargetRF = hasSelectedFields(targetFields, "RF");
-  const hasSelectedSourcePK = hasSelectedFields(sourceFields, "PK");
-  const hasSelectedTargetPK = hasSelectedFields(targetFields, "PK");
-
-  return {
-    hasSelectedSourceRF,
-    hasSelectedTargetRF,
-    hasSelectedTargetPK,
-    hasSelectedSourcePK,
-  };
+  const properties = ["RF", "PK"];
+  const validations = properties.reduce((acc, property) => {
+    acc[`hasSelectedSource${property}`] = createValidation(
+      sourceFields,
+      property
+    );
+    acc[`hasSelectedTarget${property}`] = createValidation(
+      targetFields,
+      property
+    );
+    return acc;
+  }, {});
+  return validations;
 };
-
 export default useFieldSelectValidation;
